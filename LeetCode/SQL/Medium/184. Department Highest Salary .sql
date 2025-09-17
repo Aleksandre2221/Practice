f3090804038b7ceb2e486a99bbfd8b99c15072c4
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-09-17 18:29:15 +0200

    Create 184. Department Highest Salary .sql

---
+++ LeetCode/SQL/Medium/184. Department Highest Salary .sql
@@ -0,0 +1,24 @@
+
+    -- VAR 1. Using Subquery --
+SELECT d.name Department, e.name Employee, e.salary Salary
+FROM employee e
+JOIN department d ON d.id = e.departmentid
+WHERE e.salary = (
+    SELECT MAX(e2.salary)
+    FROM employee e2
+    WHERE e2.departmentid = e.departmentid
+)
+
+
+   -- VAR 2. Using Window Function - RANK() - and - CTE - 
+WITH ranked AS (
+    SELECT d.name AS Department, 
+           e.name AS Employee, 
+           e.salary AS Salary,
+           RANK() OVER (PARTITION BY d.id ORDER BY e.salary DESC) AS rnk
+    FROM employee e
+    JOIN department d ON d.id = e.departmentid
+)
+SELECT Department, Employee, Salary
+FROM ranked
+WHERE rnk = 1;
