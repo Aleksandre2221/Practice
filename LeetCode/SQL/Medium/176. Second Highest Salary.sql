b1d5e174558966ee5812f21af03e3630347b5692
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-09-19 11:15:07 +0200

    Create 176. Second Highest Salary.sql

---
+++ LeetCode/SQL/Medium/176. Second Highest Salary.sql
@@ -0,0 +1,38 @@
+
+
+    -- VAR 1. Using Window Function - DDENSE_RANK() -- 
+SELECT MAX(Salary) AS SecondHighestSalary 
+FROM
+    (SELECT *, 
+        DENSE_RANK() OVER(ORDER BY salary DESC) rnk 
+     FROM employee) ranking
+WHERE rnk = 2
+
+
+    -- VAR 2. Scalar Subquery -- 
+SELECT (
+    SELECT DISTINCT salary
+    FROM employee 
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary 
+
+
+    -- VAR 3. 
+SELECT MAX(salary) AS SecondHighestSalary 
+FROM employee
+WHERE salary < (SELECT MAX(salary) FROM employee)
+
+
+    -- VAR 4. Using - Join 
+SELECT MAX(salary) AS SecondHighestSalary 
+FROM employee
+WHERE salary < (SELECT MAX(salary) FROM employee)
+
+
+
+
+
+
+
+
