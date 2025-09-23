74f5ed8a1f9fdd94d5806e7168087b7e52379312
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-09-23 10:31:06 +0200

    Update 175. Combine Two Tables.sql

--- LeetCode/SQL/Easy/175. Combine Two Tables.sql
+++ LeetCode/SQL/Easy/175. Combine Two Tables.sql
@@ -1,17 +1,17 @@
 
-  -- VAR 1. Left Join with ON condition - (Best Practice) 
+  -- Approach 1. Left Join with ON condition - (Best Practice) 
 SELECT p.firstname, p.lastname, a.city, a.state
 FROM person p 
 LEFT JOIN address a ON p.personId = a.personId
 
 
-  -- VAR 2. Left Join with - USING -- 
+  -- Approach 2. Left Join with - USING -- 
 SELECT p.firstname, p.lastname, a.city, a.state
 FROM person p
 LEFT JOIN address a USING(personid)
 
 
-  -- VAR 3. Natural Left Join - (Not Reccomended)
+  -- Approach 3. Natural Left Join - (Not Reccomended)
 SELECT firstname, lastname, city, state
 FROM person
 NATURAL LEFT JOIN address;
