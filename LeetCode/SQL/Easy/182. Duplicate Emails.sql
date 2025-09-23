948cba908578838ae3718f4c98898aa595c60e9a
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-09-23 10:24:02 +0200

    Update and rename Code   Testcase Testcase Test Result 182. Duplicate Emails.sql to 182. Duplicate Emails.sql

---
+++ LeetCode/SQL/Easy/182. Duplicate Emails.sql
@@ -0,0 +1,44 @@
+
+
+    -- VAR 1. Using - GROUP BY -- 
+SELECT DISTINCT email
+FROM person
+GROUP BY email
+HAVING COUNT(*) > 1
+
+
+    -- VAR 2. Using - WHERE IN - condition -- 
+SELECT DISTINCT email
+FROM person 
+WHERE email IN (
+    SELECT email
+    FROM person
+    GROUP BY email
+    HAVING COUNT(*) > 1
+)
+
+
+    -- VAR 3. Using - JOIN -- 
+SELECT DISTINCT p1.email
+FROM person p1
+JOIN person p2 
+    ON p1.email = p2.email AND p1.id <> p2.id 
+
+
+    -- VAR 4. Using window function - ROW_NUMBER() - 
+SELECT DISTINCT email
+FROM (
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY email) rn
+    FROM person 
+) sub
+WHERE rn > 1
+
+
+
+
+
+
+
+
+
+
