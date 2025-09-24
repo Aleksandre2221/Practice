3e4491b7e499ca0548bd3531eeec092adc15daf7
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-09-24 10:49:29 +0200

    Update 185. Department Top Three Salaries.sql

--- LeetCode/SQL/Hard/185. Department Top Three Salaries.sql
+++ LeetCode/SQL/Hard/185. Department Top Three Salaries.sql
@@ -11,6 +11,20 @@ WITH salary_ranking AS (
  FROM salary_ranking
  WHERE rn <= 3
  ORDER BY department, salary DESC
+ 	
+ 	
+ 	
+      -- Approach 2. Solution without using Window Function -- 
+SELECT d.name department, e.name employee, e.salary
+FROM Employee e
+JOIN Department d ON d.id = e.departmentId
+WHERE (
+    SELECT COUNT(DISTINCT e2.salary)
+    FROM Employee e2
+    WHERE e2.departmentId = e.departmentId
+      AND e2.salary > e.salary
+) < 3
+ORDER BY department, salary DESC;
 
 
 
