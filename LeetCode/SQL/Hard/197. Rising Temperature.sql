be2ab9a5136c9179a63b349a77337de0992d36bb
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-09-25 12:02:05 +0200

    Update 197. Rising Temperature.sql

--- LeetCode/SQL/Hard/197. Rising Temperature.sql
+++ LeetCode/SQL/Hard/197. Rising Temperature.sql
@@ -1,6 +1,9 @@
 
+            -- Warning: You cant substract dates directly in all RDBMS, is some of them you have to use function - DATEDIFF -- 
 
-      -- Appraoch 1. Using - CTE - and Window Functions - LAG() / LEAD() -- 
+
+
+      -- Appraoch 1. Using - CTE - and Window Functions - LAG() / LEAD() -- (For - PostgreSQL/Oracle)
 WITH prev_day_temp AS (
     SELECT *,
         LAG(temperature) OVER(ORDER BY recordDate) prev_temp,
@@ -12,9 +15,23 @@ FROM prev_day_temp
 WHERE recordDate - prev_day = 1 
     AND temperature > prev_temp
 
-  
 
-      -- Approach 2. Using - CASE...WHEN - and Window Functions - LAG() / LEAD() -- 
+      
+      -- Approach 2. Using - CTE - and Window Functions - LAG() / LEAD() -- (For - MySQL/SQL Server)
+WITH prev_day_temp AS (
+    SELECT *,
+        LAG(temperature) OVER(ORDER BY recordDate) prev_temp,
+        LAG(recordDate) OVER(ORDER BY recordDate) prev_day
+    FROM weather
+)
+SELECT id
+FROM prev_day_temp
+WHERE DATEDIFF(recordDate, prev_day) = 1 
+    AND temperature > prev_temp
+
+      
+
+      -- Approach 3. Using - CASE...WHEN - and Window Functions - LAG() / LEAD() -- (For - PostgreSQL/Oracle)
 SELECT id
 FROM (
     SELECT id,
@@ -27,3 +44,23 @@ FROM (
     FROM weather
 ) AS check_temp
 WHERE is_increasing = 1;
+
+
+
+      -- Approach 4. Using - Self-Join -- 
+SELECT w2.id
+FROM weather w1
+JOIN weather w2
+    ON w2.recordDate = DATE_ADD(w1.recordDate, INTERVAL '1 DAY')
+        AND w2.temperature > w1.temperature
+
+      
+            
+                  
+
+
+
+
+
+
+
