8a44c59ce07cb8b9349cc48854ae3237ffaad04e
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-09-26 12:27:13 +0200

    Update 550. Game Play Analysis IV.sql

--- LeetCode/SQL/Medium/550. Game Play Analysis IV.sql
+++ LeetCode/SQL/Medium/550. Game Play Analysis IV.sql
@@ -1,6 +1,25 @@
 
 
-        -- Approach 1. Using - CTE - with Window Functions - ROW_NUMBER / LEAD
+		-- Appraoch 1. Using - WHERE IN - condition -- 
+SELECT ROUND(COUNT(player_id)::numeric / (SELECT COUNT(DISTINCT player_id) FROM activity), 2) fraction
+FROM activity 
+WHERE (player_id, event_date - INTERVAL '1 DAY') IN (
+  SELECT player_id, MIN(event_date) first_date FROM activity GROUP BY player_id
+  )
+
+
+	 -- Approach 2. Using Subquery and - LEFT JOIN --
+SELECT ROUND(COUNT(t2.player_id)::numeric / COUNT(t1.player_id), 2) fraction
+FROM (
+    SELECT player_id, MIN(event_date) first_login
+    FROM activity
+    GROUP BY player_id
+) t1
+LEFT JOIN activity t2 ON t1.player_id = t2.player_id AND t2.event_date = t1.first_login + 1
+
+	
+
+        -- Approach 3. Using - CTE - with Window Functions - ROW_NUMBER / LEAD
 WITH t1 AS (
   SELECT *, ROW_NUMBER() OVER(PARTITION BY player_id ORDER BY event_date) AS rn,
 	LEAD(event_date) OVER(PARTITION BY player_id) - event_date AS date_diff
@@ -12,8 +31,9 @@ SELECT
 		, 2) AS fraction 
 FROM t1 
 
+	
   
-        -- Approach 2. Using only - CTE - without Window Functions -- 
+        -- Approach 4. Using only - CTE - without Window Functions -- 
 WITH 
 	min_date AS (
       SELECT player_id, MIN(event_date) min_date
