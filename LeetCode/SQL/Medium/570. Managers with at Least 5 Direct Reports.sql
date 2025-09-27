632e44dc8013dfe7870a322a6e07722fe34e4124
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-09-27 11:01:29 +0200

    Update 570. Managers with at Least 5 Direct Reports.sql

--- LeetCode/SQL/Medium/570. Managers with at Least 5 Direct Reports.sql
+++ LeetCode/SQL/Medium/570. Managers with at Least 5 Direct Reports.sql
@@ -11,6 +11,25 @@ WHERE id IN (
 )
 
 
+        -- Appraoch 2. Using - Self-Join -- 
+SELECT e1.name
+FROM Employee e1
+JOIN employee e2 ON e1.id = e2.managerid
+GROUP BY e1.id, e1.name
+HAVING COUNT(*) >= 5
+
+
+  
+        -- Approach 3. Using - WHERE EXISTS - condition -- 
+SELECT e1.name
+FROM employee e1
+WHERE EXISTS (
+  SELECT 1
+  FROM employee e2
+  WHERE e2.managerid = e1.id
+  GROUP BY e2.managerid 
+  HAVING COUNT(*) >=5
+)
 
 
 
