2df291241f43e46db08bfdd62440ace459361b70
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-09-30 11:47:37 +0200

    Update 512. Game Play Analysis II.sql

--- LeetCode/SQL/Easy/512. Game Play Analysis II.sql
+++ LeetCode/SQL/Easy/512. Game Play Analysis II.sql
@@ -7,11 +7,37 @@ ORDER BY player_id, event_date
 
   
 
-        -- Approach 2. Using  Window Function - ROW_NUMBER() -- 
+        -- Approach 2. Using  Subquery and Window Function - ROW_NUMBER() -- 
 SELECT player_id, device_id
 FROM (
-  	SELECT *, 
-  		ROW_NUMBER() OVER(PARTITION BY player_id ORDER BY event_date) rn
+  	SELECT *, ROW_NUMBER() OVER(PARTITION BY player_id ORDER BY event_date) rn
   	FROM activity
 ) sub 
 WHERE rn = 1
+
+        
+
+        -- Approach 3. Using - CTE and Window Function - ROW_NUMBER() -- 
+WITH row_num AS (
+  SELECT *, ROW_NUMBER() OVER(PARTITION BY player_id ORDER BY event_date) rn
+  FROM activity
+)
+SELECT player_id, device_id
+FROM row_num 
+WHERE rn = 1
+
+
+
+        -- Approach 4. Using - WHERE IN - condition -- 
+SELECT player_id, device_id
+FROM activity 
+WHERE (player_id, event_date) IN (
+  SELECT player_id, MIN(event_date) event_date
+  FROM activity 
+  GROUP BY player_id)
+
+
+
+
+
+
