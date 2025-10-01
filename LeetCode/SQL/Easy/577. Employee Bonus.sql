7c1d4f40d89372643fc836795ec18394b88fe347
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-01 23:03:23 +0200

    Update 577. Employee Bonus.sql

--- LeetCode/SQL/Easy/577. Employee Bonus.sql
+++ LeetCode/SQL/Easy/577. Employee Bonus.sql
@@ -5,3 +5,12 @@ SELECT e.name, b.bonus
 FROM employee e 
 LEFT JOIN bonus b ON b.empid = e.empid
 WHERE b.bonus < 1000 OR b.bonus IS NULL
+
+
+
+         -- Approach 2. Using - COALESCE -- 
+SELECT e.name, b.bonus
+FROM employee e
+LEFT JOIN bonus b ON b.empid = e.empid
+WHERE COALESCE(b.bonus, 0) < 1000;
+
