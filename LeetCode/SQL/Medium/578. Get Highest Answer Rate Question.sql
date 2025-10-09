0abd089fe06b24928ed759184c1fb067f820b953
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-09 11:19:45 +0200

    Update 578. Get Highest Answer Rate Question.sql

--- LeetCode/SQL/Medium/578. Get Highest Answer Rate Question.sql
+++ LeetCode/SQL/Medium/578. Get Highest Answer Rate Question.sql
@@ -2,7 +2,7 @@
 
          -- Approach 1. Using - GROUP BY and - FILTER -- 
 SELECT question_id, 
-    COUNT(*) FILTER(WHERE answer_id IS NOT NULL)
+    COUNT(*) FILTER(WHERE answer_id IS NOT NULL)::numeric
       / COUNT(*) AS answer_rate
 FROM survey_log
 GROUP BY question_id
@@ -13,9 +13,26 @@ LIMIT 1;
 
          -- Approach 2. Using - GROUP BY with - HAVING -- 
 SELECT question_id, 
-    COUNT(*) FILTER(WHERE answer_id IS NOT NULL)
+    COUNT(*) FILTER(WHERE answer_id IS NOT NULL)::numeric
       / COUNT(*) AS answer_rate
 FROM survey_log
 GROUP BY question_id
 ORDER BY answer_rate DESC
 LIMIT 1;
+
+
+
+         -- Approach 3. Using - CASE...WHEN - within - SUM function -- 
+SELECT question_id, 
+    (SUM(CASE WHEN answer_id IS NOT NULL THEN 1 ELSE 0 END)::numeric 
+      / COUNT(*)) AS answer_rate
+FROM survey_log
+GROUP BY question_id
+ORDER BY answer_rate DESC
+LIMIT 1;
+
+
+
+
+
+
