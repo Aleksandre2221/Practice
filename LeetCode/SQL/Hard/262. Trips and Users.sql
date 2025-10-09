1c7dc78ffe8f1e746c57d82050018b752eae9ce2
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-09 14:45:56 +0200

    Update 262. Trips and Users.sql

--- LeetCode/SQL/Hard/262. Trips and Users.sql
+++ LeetCode/SQL/Hard/262. Trips and Users.sql
@@ -17,5 +17,18 @@ ORDER BY t.request_at;
 
 
 
-
-
+         -- Approach 2. Using one - JOIN but multiple - WHERE / AND - conditions (less elegant solution, but it still works)
+SELECT t.request_at "Day",
+	ROUND(	
+	        SUM(CASE WHEN t.status IN ('cancelled_by_driver', 'cancelled_by_client') THEN 1 ELSE 0 END)::numeric 
+                  / COUNT(*)
+             , 2) "Cancellation Rate"
+FROM users u 
+JOIN trips t ON (t.client_id = u.users_id OR t.driver_id = u.users_id)
+WHERE 
+    u.banned <> 'Yes'
+    AND t.client_id NOT IN (SELECT users_id FROM users WHERE banned = 'Yes')
+    AND t.driver_id NOT IN (SELECT users_id FROM users WHERE banned = 'Yes')
+    AND t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.request_at
+ORDER BY t.request_at;
