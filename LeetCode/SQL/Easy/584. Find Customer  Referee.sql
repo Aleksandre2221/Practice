2da2893f1b82423f08147f02a3116f13f0813b07
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-10 11:10:46 +0200

    Update 584. Find Customer  Referee.sql

--- LeetCode/SQL/Easy/584. Find Customer  Referee.sql
+++ LeetCode/SQL/Easy/584. Find Customer  Referee.sql
@@ -1,13 +1,51 @@
 
 
-         -- Approach 1. Using - WHERE - condition -- 
+         -- Approach 1. Using - WHERE and - COALESCE --
+SELECT name
+FROM customer
+WHERE COALESCE(referee_id, 0) <> 2;
+
+
+
+
+         -- Approach 2. Using - WHERE - with multiple conditions -- 
 SELECT name  
 FROM customer
-WHERE referee_id <> 2 OR referee_id IS NULL
+WHERE referee_id <> 2 OR referee_id IS NULL;
+
+
+         
+
+         -- Approach 3. Using - WHERE with - NOT IN condition -- 
+SELECT name  
+FROM customer 
+WHERE id NOT IN (
+  SELECT id  
+  FROM customer 
+  WHERE referee_id= 2); 
 
 
 
-         -- Approach 2. Using - Self JOIN - with conditions -- 
-SELECT DISTINCT c2.name  
-FROM customer c1
-JOIN customer c2 ON c2.referee_id <> 2 OR c2.referee_id IS NULL
+
+         -- Approach 4. Using condition - IS DISTINCT FROM - (only in PostgreSQL for null-safe inequality) -- 
+SELECT name
+FROM customer
+WHERE referee_id IS DISTINCT FROM 2;
+
+
+
+
+         -- Approach 5. Using condition - NOT <=> - (only in MySQL for null-safe inequality) -- 
+SELECT name
+FROM customer
+WHERE NOT referee_id <=> 2;
+
+
+
+
+
+
+
+
+
+
