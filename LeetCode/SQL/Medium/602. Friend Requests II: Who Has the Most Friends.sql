1657beee1e2fd0c5552b82f6a440fa74333a6eca
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-11 11:40:19 +0200

    Update 602. Friend Requests II: Who Has the Most Friends.sql

--- LeetCode/SQL/Medium/602. Friend Requests II: Who Has the Most Friends.sql
+++ LeetCode/SQL/Medium/602. Friend Requests II: Who Has the Most Friends.sql
@@ -10,3 +10,26 @@ FROM (
 GROUP BY id  
 ORDER BY num DESC  
 LIMIT 1
+
+
+
+         -- Appraoch 2. Withoout - UNION ALL -- 
+WITH 
+	req_cnt AS (
+      SELECT requester_id, COUNT(*) req_cnt
+      FROM requestaccepted
+      GROUP BY requester_id), 
+      
+     acc_cnt AS (
+       SELECT accepter_id, COUNT(*) acc_cnt
+       FROM requestaccepted
+       GROUP BY accepter_id)
+
+SELECT * 
+FROM (
+  SELECT COALESCE(requester_id, accepter_id) id , COALESCE(req_cnt, 0) + COALESCE(acc_cnt, 0) num
+  FROM req_cnt 
+  FULL JOIN acc_cnt ON acc_cnt.accepter_id = req_cnt.requester_id
+)
+ORDER BY num DESC  
+LIMIT 1
