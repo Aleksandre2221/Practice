d1771f949b03955538f1c4e025d26598c91b7d05
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-12 21:48:52 +0200

    Update 614. Second Degree Follower.sql

--- LeetCode/SQL/Medium/614. Second Degree Follower.sql
+++ LeetCode/SQL/Medium/614. Second Degree Follower.sql
@@ -6,3 +6,18 @@ FROM follow f1
 JOIN follow f2 ON f1.followee = f2.follower
 GROUP BY f1.followee
 ORDER BY name;
+
+
+
+
+         -- Approach 2. using - CTE with Window Functions -- 
+WITH counts AS (
+  SELECT 
+    followee, COUNT(*) OVER(PARTITION BY followee) followee_cnt,
+    follower, COUNT(*) OVER(PARTITION BY follower) follower_cnt
+  FROM follow
+)
+SELECT DISTINCT c1.followee AS "name", c1.followee_cnt AS num
+FROM counts c1
+JOIN counts c2 ON c1.followee = c2.follower
+ORDER BY name;
