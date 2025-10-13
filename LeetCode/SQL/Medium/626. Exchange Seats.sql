24755213b44e9542963a88c3d6665c4ee58ff632
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-13 11:13:11 +0200

    Update 626. Exchange Seats.sql

--- LeetCode/SQL/Medium/626. Exchange Seats.sql
+++ LeetCode/SQL/Medium/626. Exchange Seats.sql
@@ -10,6 +10,16 @@ FROM (
       END swapped
   FROM seat)
 
+  
+
+
+          -- Approach 2. Using - Subquery and - CASE...WHEN condition within - COALESCE --  
+SELECT id, 
+	COALESCE(
+      	(CASE WHEN id % 2 = 0 THEN LAG(student) OVER(ORDER BY id) ELSE LEAD(student) OVER(ORDER BY id) END),
+      	(SELECT student FROM seat ORDER BY id DESC LIMIT 1) 
+    ) student 
+FROM seat 
 
 
 
