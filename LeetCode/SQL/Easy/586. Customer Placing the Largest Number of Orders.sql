cf3d982d71a6f427a199749d4beaab22e3deefcd
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-14 10:37:47 +0200

    Update 586. Customer Placing the Largest Number of Orders.sql

--- LeetCode/SQL/Easy/586. Customer Placing the Largest Number of Orders.sql
+++ LeetCode/SQL/Easy/586. Customer Placing the Largest Number of Orders.sql
@@ -1,8 +1,17 @@
 
 
-         -- The Best Approach. Using - COUNT(*) within - ORDER BY clause --   
+         -- Approach 1. Using - COUNT(*) within - ORDER BY clause --   
 SELECT customer_number
 FROM orders 
 GROUP BY customer_number
 ORDER BY COUNT(*) DESC 
 LIMIT 1;
+
+
+
+         -- Approach 2. Using - TOP - (only fro - SQL Server / T-SQL) -- 
+SELECT TOP 1
+FROM orders 
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC;
+
