9f6391fdc4764dffabdb22485e648922c2f2e1e7
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-14 10:51:47 +0200

    Update 596. Classes With at Least 5 Students.sql

--- LeetCode/SQL/Easy/596. Classes With at Least 5 Students.sql
+++ LeetCode/SQL/Easy/596. Classes With at Least 5 Students.sql
@@ -5,3 +5,14 @@ SELECT class
 FROM courses
 GROUP BY class
 HAVING COUNT(*) >= 5
+
+
+         -- Approach 2. Using - Subquery and - WHERE - condition -- 
+SELECT class
+FROM (
+  SELECT class, COUNT(*) AS total
+  FROM courses
+  GROUP BY class
+) AS sub
+WHERE total >= 5;
+
