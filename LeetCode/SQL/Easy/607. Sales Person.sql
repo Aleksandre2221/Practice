b26fb3e0acef478b912546eae4ae8083712f15d5
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-15 11:54:46 +0200

    Update 607. Sales Person.sql

--- LeetCode/SQL/Easy/607. Sales Person.sql
+++ LeetCode/SQL/Easy/607. Sales Person.sql
@@ -3,13 +3,12 @@
          -- Approach 1. Using - NOT IN - condition with multiple - LEFT JOIN -- 
 SELECT name  
 FROM salesperson 
-WHERE name NOT IN (
-  SELECT s.name
+WHERE sales_id NOT IN (
+  SELECT s.sales_id
   FROM salesperson s 
   LEFT JOIN orders o ON o.sales_id = s.sales_id
   LEFT JOIN company c ON c.com_id = o.com_id
   WHERE c.name = 'RED');
-
 
 
 
@@ -22,7 +21,6 @@ WHERE s.sales_id NOT IN (
     JOIN company c ON o.com_id = c.com_id
     WHERE c.name = 'RED'
 );
-
 
 
 
