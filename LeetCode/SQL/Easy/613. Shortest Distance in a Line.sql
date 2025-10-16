f4c6b00961c4c6014dad5cf36d1bf14c948376eb
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-16 11:05:57 +0200

    Update 613. Shortest Distance in a Line.sql

--- LeetCode/SQL/Easy/613. Shortest Distance in a Line.sql
+++ LeetCode/SQL/Easy/613. Shortest Distance in a Line.sql
@@ -4,3 +4,15 @@
 SELECT MIN(ABS(p1.x - p2.x)) AS shortest
 FROM point p1
 JOIN point p2 ON p1.x <> p2.x;
+
+
+
+         -- Approach 2. Without using - MIN --
+SELECT distance shortest
+FROM (
+  SELECT *, ABS(p1.x - p2.x) distance
+  FROM point p1 
+  JOIN point p2 ON p1.x <> p2.x
+  ORDER BY distance 
+  LIMIT 1
+);
