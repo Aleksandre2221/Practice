f1287e907e0ab1fbb4455b8518b68cc537291046
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-17 11:34:58 +0200

    Update 1050. Actors and Directors Who Cooperated At Least Three Times.sql

--- LeetCode/SQL/Easy/1050. Actors and Directors Who Cooperated At Least Three Times.sql
+++ LeetCode/SQL/Easy/1050. Actors and Directors Who Cooperated At Least Three Times.sql
@@ -1,7 +1,19 @@
 
 
-         -- Approach 1. Using - GROUP BY with - HAVING -- 
+         -- The Simplest Approach. Using - GROUP BY with - HAVING -- 
 SELECT actor_id, director_id
 FROM ActorDirector
 GROUP BY actor_id, director_id
 HAVING COUNT(*) >= 3;
+
+
+
+         -- Approach 2. Using - CTE -- 
+WITH collab_cnt AS (
+    SELECT actor_id, director_id, COUNT(*) AS cnt
+    FROM ActorDirector
+    GROUP BY actor_id, director_id
+)
+SELECT actor_id, director_id
+FROM collab_cnt
+WHERE cnt >= 3;
