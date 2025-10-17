ac6a1e75dd73d38a6352a08cc5dc168a3d482600
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-17 11:23:17 +0200

    Update 627. Swap Salary.sql

--- LeetCode/SQL/Easy/627. Swap Salary.sql
+++ LeetCode/SQL/Easy/627. Swap Salary.sql
@@ -6,3 +6,14 @@ SET sex = CASE
     WHEN sex = 'f' THEN 'm' 
     ELSE 'f' 
 END;
+
+
+
+
+         -- Approach 2. Using - CASE with two - WHEN - conditions (handles cases with null values) -- 
+UPDATE salary
+SET sex = CASE 
+    WHEN sex = 'f' THEN 'm'
+    WHEN sex = 'm' THEN 'f'
+    ELSE sex 
+END;
