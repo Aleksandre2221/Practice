1f4ce1bf94c2b925dcfb215b63a7863d57b0e95a
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-18 12:15:12 +0200

    Create 1084. Sales Analysis III.sql

---
+++ LeetCode/SQL/Easy/1084. Sales Analysis III.sql
@@ -0,0 +1,22 @@
+
+
+         -- Approach 1. Using - NOT IN condition -- 
+SELECT DISTINCT p.product_id, p.product_name
+FROM sales s1 
+JOIN product p ON p.product_id = s1.product_id 
+WHERE p.product_id NOT IN (
+  SELECT product_id 
+  FROM sales 
+  WHERE sale_date NOT BETWEEN '2019-01-01'::date AND '2019-03-31'::date);
+
+
+
+
+         -- Approach 2. Using - NOT EXISTS nodition -- 
+SELECT DISTINCT p.product_id, p.product_name
+FROM sales s1 
+JOIN product p ON p.product_id = s1.product_id 
+WHERE p.product_id NOT IN (
+  SELECT product_id 
+  FROM sales 
+  WHERE sale_date NOT BETWEEN '2019-01-01'::date AND '2019-03-31'::date);
