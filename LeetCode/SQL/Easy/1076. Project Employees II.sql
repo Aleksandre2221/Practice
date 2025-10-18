809261e4ca0dc3c211b2ac514e8a265fee224d6d
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-18 11:04:44 +0200

    Update 1076. Project Employees II.sql

--- LeetCode/SQL/Easy/1076. Project Employees II.sql
+++ LeetCode/SQL/Easy/1076. Project Employees II.sql
@@ -6,6 +6,18 @@ FROM project
 GROUP BY project_id
 HAVING COUNT(*) = (
     SELECT MAX(cnt) 
-    FROM (SELECT COUNT(*) cnt FROM project GROUP BY project_id)
+    FROM (SELECT COUNT(*) cnt FROM project GROUP BY project_id) sub
   );
+
+
+
+         -- Appraoch 2. Using - Subquery with Window Function - RANK() -- 
+SELECT project_id 
+FROM (
+  SELECT project_id, RANK() OVER(ORDER BY COUNT(*) DESC) rnk
+  FROM project
+  GROUP BY project_id
+) sub
+WHERE rnk = 1;
+
 
