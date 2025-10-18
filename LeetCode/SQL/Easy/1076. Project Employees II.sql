3c979252fed0ef8a71f29f847115fb9e315426da
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-18 10:49:57 +0200

    Refactor SQL query to simplify project selection

--- LeetCode/SQL/Easy/1076. Project Employees II.sql
+++ LeetCode/SQL/Easy/1076. Project Employees II.sql
@@ -1,9 +1,9 @@
 
 
          -- Approach 1. Using - GROUP BY / ORDER BY / LIMIT -- 
-SELECT p.project_id 
-FROM employee e  
-JOIN project p ON p.employee_id = e.employee_id
-GROUP BY p.project_id
+SELECT project_id 
+FROM project 
+GROUP BY project_id
 ORDER BY COUNT(*) DESC 
 LIMIT 1;
+
