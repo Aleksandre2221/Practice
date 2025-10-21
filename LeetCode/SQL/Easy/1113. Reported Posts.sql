4b741e8b59236cb9f63c0be0a7b2651506e32dab
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-21 10:04:56 +0200

    Add SQL query for reported posts on a specific date

---
+++ LeetCode/SQL/Easy/1113. Reported Posts.sql
@@ -0,0 +1,10 @@
+
+
+         -- Approach 1. Using multiple - WHERE conditions with - GROUP BY and - COUNT(DISTINCT..) --  
+SELECT extra, COUNT(DISTINCT post_id) report_count
+FROM actions
+WHERE 
+	action_date = '2019-07-04'::date 
+  AND action = 'report'
+	AND extra IS NOT NULL 
+GROUP BY extra;
