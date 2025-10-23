34b7caa6a559ca141861f29e4a96fdd86f45af0b
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-23 11:37:09 +0200

    Update 1322. Ads Performance.sql

--- LeetCode/SQL/Easy/1322. Ads Performance.sql
+++ LeetCode/SQL/Easy/1322. Ads Performance.sql
@@ -16,3 +16,21 @@ GROUP BY ad_id;
 
 
 
+           -- Approach 2. Using - CTE and - COUNT with (CASE....WHEN) condionts -- 
+WITH ad_stats AS (
+    SELECT ad_id,
+        COUNT(CASE WHEN action = 'Clicked' THEN 1 END) total_clicked,
+        COUNT(CASE WHEN action = 'Viewed'  THEN 1 END) total_viewed
+    FROM ads_actions
+    GROUP BY ad_id
+)
+SELECT ad_id,
+    CASE 
+        WHEN total_clicked = 0 THEN 0
+        ELSE ROUND(total_clicked * 100.0 / (total_clicked + total_viewed), 2)
+    END AS ctr
+FROM ad_stats;
+
+
+
+
