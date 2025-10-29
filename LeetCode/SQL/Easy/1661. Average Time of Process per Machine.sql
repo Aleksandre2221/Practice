8c58c7cbee72c169153adc88ade572ad33daed13
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-29 10:13:49 +0100

    Update 1661. Average Time of Process per Machine.sql

--- LeetCode/SQL/Easy/1661. Average Time of Process per Machine.sql
+++ LeetCode/SQL/Easy/1661. Average Time of Process per Machine.sql
@@ -5,7 +5,7 @@ SELECT machine_id,
        ROUND(
          	(AVG(timestamp) FILTER (WHERE activity_type = 'end') -
           		AVG(timestamp) FILTER (WHERE activity_type = 'start'))::numeric
-         , 3) AS avg_processing_time
+         , 3) AS processing_time 
 FROM activity
 GROUP BY machine_id;
 
