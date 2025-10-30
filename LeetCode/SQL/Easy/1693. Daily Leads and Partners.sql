6c8631fa8aad3e39cab574a6d2715c345d5a01ae
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-10-30 09:22:44 +0100

    Create 1693. Daily Leads and Partners.sql

---
+++ LeetCode/SQL/Easy/1693. Daily Leads and Partners.sql
@@ -0,0 +1,8 @@
+
+
+         -- The Best Approach. Using - GROUP BY with - COUNT(DISTINCT....) -- 
+SELECT date_id, make_name, 
+    COUNT(DISTINCT lead_id) unique_leads,
+    COUNT(DISTINCT partner_id) unique_partners
+FROM dailysales
+GROUP BY date_id, make_name;
