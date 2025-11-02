f8010ee2e1d33e2b4350c5147d34e87ac2f8de38
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-11-02 09:29:57 +0100

    Update 1978. Employees Whose Manager Left the Company.sql

--- LeetCode/SQL/Easy/1978. Employees Whose Manager Left the Company.sql
+++ LeetCode/SQL/Easy/1978. Employees Whose Manager Left the Company.sql
@@ -5,3 +5,12 @@ SELECT employee_id
 FROM employees
 WHERE salary < 30000 AND manager_id NOT IN (SELECT employee_id FROM employees)
 ORDER BY employee_id;
+
+
+
+         -- Approach 2. Using - LEFT JOIN -- 
+SELECT e1.employee_id
+FROM employees e1
+LEFT JOIN employees e2 ON e1.manager_id = e2.employee_id
+WHERE e1.salary < 30000 AND e2.employee_id IS NULL AND e1.manager_id IS NOT NULL
+ORDER BY employee_id;
