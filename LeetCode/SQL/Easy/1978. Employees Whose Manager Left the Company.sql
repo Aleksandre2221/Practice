d4a1a13aeae7182bdfb4af1da3174ae008af5d84
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-11-02 09:28:32 +0100

    Create 1978. Employees Whose Manager Left the Company.sql

---
+++ LeetCode/SQL/Easy/1978. Employees Whose Manager Left the Company.sql
@@ -0,0 +1,7 @@
+
+
+         -- Approach 1. Using - WHERE NOT IN condition -- 
+SELECT employee_id
+FROM employees
+WHERE salary < 30000 AND manager_id NOT IN (SELECT employee_id FROM employees)
+ORDER BY employee_id;
