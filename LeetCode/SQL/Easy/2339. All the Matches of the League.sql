640dd3adca160f13e98661853786e207f7b2c99c
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-11-03 11:06:52 +0100

    Create 2339. All the Matches of the League.sql

---
+++ LeetCode/SQL/Easy/2339. All the Matches of the League.sql
@@ -0,0 +1,6 @@
+
+
+         -- The Best Approach. Using - Cross Join and - WHERE condition -- 
+SELECT t1.team_name home_team, t2.team_name away_team 
+FROM teams t1, teams t2
+WHERE t1.team_name <> t2.team_name;
