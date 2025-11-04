aaef7de42ca385c9b797d3630a8e9442d8533334
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-11-04 11:48:03 +0100

    Create  1555. Bank Account Summary.sql

---
+++ LeetCode/SQL/Medium/1555. Bank Account Summary.sql
@@ -0,0 +1,42 @@
+
+
+         -- Approach 1. Using - UNION ALL within - LEFT JOIN -- 
+SELECT u.user_id, u.user_name,
+    u.credit + COALESCE(SUM(t.amount_change), 0) credit,
+    CASE 
+        WHEN u.credit + COALESCE(SUM(t.amount_change), 0) < 0 THEN 'Yes'
+        ELSE 'No'
+    END credit_limit_breached
+FROM users u
+LEFT JOIN (
+    SELECT paid_by AS user_id, -amount amount_change FROM transaction
+    UNION ALL
+    SELECT paid_to AS user_id,  amount amount_change FROM transaction
+) t ON t.user_id = u.user_id
+GROUP BY u.user_id, u.user_name, u.credit;
+
+
+
+
+         -- Approach 2. Using two - CTE -- 
+WITH 
+    paid AS (
+        SELECT paid_by user_id, SUM(amount) total_paid
+        FROM transaction 
+        GROUP BY paid_by
+    ),
+    received AS (
+        SELECT paid_to user_id, SUM(amount) total_received
+        FROM transaction 
+        GROUP BY paid_to
+)
+SELECT u.user_id, u.user_name,
+	u.credit - COALESCE(p.total_paid, 0) + COALESCE(r.total_received, 0) credit,
+    CASE 
+        WHEN (u.credit - COALESCE(p.total_paid, 0) + COALESCE(r.total_received, 0)) < 0 
+        THEN 'Yes'
+        ELSE 'No'
+    END credit_limit_breached
+FROM users u
+LEFT JOIN paid p ON p.user_id = u.user_id
+LEFT JOIN received r ON r.user_id = u.user_id;
