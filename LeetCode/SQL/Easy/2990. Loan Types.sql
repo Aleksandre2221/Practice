2273de756dd68a213fcce23d1ba311a34e157aee
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-11-07 11:11:29 +0100

    Add self-join query for loan types
    Added a self-join approach to retrieve users with both 'Refinance' and 'Mortgage' loan types.

--- LeetCode/SQL/Easy/2990. Loan Types.sql
+++ LeetCode/SQL/Easy/2990. Loan Types.sql
@@ -7,3 +7,14 @@ GROUP BY user_id
 HAVING SUM(CASE WHEN loan_type = 'Refinance' THEN 1 END) > 0 
     AND SUM(CASE WHEN loan_type = 'Mortgage' THEN 1 END) > 0
 ORDER BY user_id;
+
+
+
+
+         -- Approach 2. Using - Self-Join with - WHERE conditions --
+SELECT DISTINCT a.user_id
+FROM Loans a
+JOIN Loans b ON a.user_id = b.user_id
+WHERE a.loan_type = 'Refinance' AND b.loan_type = 'Mortgage'
+ORDER BY a.user_id;
+
