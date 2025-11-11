61586adbab913f0169618d58505f6162099da299
Aleksandre2221 <165426119+Aleksandre2221@users.noreply.github.com>
2025-11-11 11:09:36 +0100

    Update 1699. Number of Calls Between Two Persons.sql

--- LeetCode/SQL/Medium/1699. Number of Calls Between Two Persons.sql
+++ LeetCode/SQL/Medium/1699. Number of Calls Between Two Persons.sql
@@ -8,3 +8,15 @@ SELECT
     SUM(duration) total_duration
 FROM calls
 GROUP BY person1, person2;
+
+
+
+  
+         -- Approach 2. Using - LEAST() and - GREATEST() -- 
+  SELECT
+    LEAST(from_id, to_id) AS person1,
+    GREATEST(from_id, to_id) AS person2,
+    COUNT(*) AS call_count,
+    SUM(duration) AS total_duration
+FROM calls
+GROUP BY person1, person2;
