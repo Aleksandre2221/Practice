fc31d0b6dc236283e3e1febea23f27f16ea348fc
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2025-11-15 12:44:00 +0100

    Create 2051. The Category of Each Member in the Store.sql

---
+++ LeetCode/SQL/Medium/2051. The Category of Each Member in the Store.sql
@@ -0,0 +1,14 @@
+
+
+         -- Approach 1. Using two - LEFT JOIN and - CASE...WHEN conditions -- 
+SELECT m.member_id, m.name,
+    CASE
+        WHEN COUNT(v.visit_id) = 0 THEN 'Bronze'
+        WHEN 100 * COUNT(p.visit_id) / COUNT(v.visit_id) >= 80 THEN 'Diamond'
+        WHEN 100 * COUNT(p.visit_id) / COUNT(v.visit_id) >= 50 THEN 'Gold'
+        ELSE 'Silver'
+    END category
+FROM members m
+LEFT JOIN visits v ON m.member_id = v.member_id
+LEFT JOIN purchases p ON v.visit_id = p.visit_id
+GROUP BY m.member_id;
